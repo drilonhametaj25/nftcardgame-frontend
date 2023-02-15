9f3d1974e6197813a9030f160aecb13df948db6b
drilon <drilonhametaj25@gmail.com>
2023-02-15 17:59:32 +0100

    home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
+import { PageHOC } from '../components';
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
-      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
     </div>
   )
 };
 
-export default Home;
+export default PageHOC(Home, <>Welcome to Avax Gods <br/> a Web3 NFT Game</>,<>Connect your wallet to start playing!</>);
