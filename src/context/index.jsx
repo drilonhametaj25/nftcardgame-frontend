bc462010d3826edc4ed21d141ac4dcece589965c
drilon <drilonhametaj25@gmail.com>
2023-02-16 15:50:14 +0100

    connect to wallet and set to state

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,5 +1,5 @@
 import React, {
-    createContext,
+  createContext,
   CreateContext,
   useContext,
   useEffect,
@@ -9,43 +9,55 @@ import React, {
 import { ethers, Signer } from "ethers";
 import Web3Model from "web3modal";
 import { useNavigate } from "react-router-dom";
+import { ABI, ADDRESS } from "../contract";
 
 const GlobalContext = createContext();
 
-export const GlobalContextProvider = ({children}) => {
-    // Connect and interact with smartcontract
-    const [walletAddress, setWalletAddress] = useState('')
+export const GlobalContextProvider = ({ children }) => {
+  // Connect and interact with smartcontract
+  const [walletAddress, setWalletAddress] = useState("");
+  const [provider, setProvider] = useState("");
+  const [contract, setContract] = useState("");
 
-    const updateCurrentWalletAddress = async () => {
-        const accounts = await window.ethereum.request({
-            method: 'eth_requestAccounts'
-        });
-        console.log(accounts)
-    }
+  // Set Wallet Address to the state
+  const updateCurrentWalletAddress = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    if(accounts) setWalletAddress(accounts[0])
+    
+  };
 
-    useEffect(() => {
-        updateCurrentWalletAddress();
-    }, [])
+  useEffect(() => {
+    updateCurrentWalletAddress();
 
-    useEffect(() => {
-        const setSmartContractAndProvider = async () => {
-            const web3modal = new Web3Model()
-            const connection = await web3modal.connect()
-            const newProvider = new ethers.providers.Web3Provider(connection)
-            const signer = newProvider.signer()
-            const newContract = new ethers.Contract();
-        }
+    window.ethereum.on('accountsChanged', updateCurrentWalletAddress)
+  }, []);
 
-        setSmartContractAndProvider()
-    })
-    // Pass object with every single component in the application
-    return (
-        <GlobalContext.Provider value={{
-            demo: 'test'
-        }}>
-            {children}
-        </GlobalContext.Provider>
-    )
-}
+  // Used fot set the smart contract to the state
+  useEffect(() => {
+    const setSmartContractAndProvider = async () => {
+      const web3modal = new Web3Model();
+      const connection = await web3modal.connect();
+      const newProvider = new ethers.providers.Web3Provider(connection);
+      const signer = newProvider.signer();
+      const newContract = new ethers.Contract(ADDRESS, ABI, signer);
+      setProvider(newProvider)
+      setContract(newContract)
+    };
 
-export const useGlobalContext = () => useContext(GlobalContext)
+    setSmartContractAndProvider();
+  });
+  // Pass object with every single component in the application
+  return (
+    <GlobalContext.Provider
+      value={{
+        demo: "test",
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
