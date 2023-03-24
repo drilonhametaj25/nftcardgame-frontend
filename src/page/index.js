0e884e561cb44bc1730f9adf0f16783ebcdd3766
drilon <drilonhametaj25@gmail.com>
2023-03-24 14:55:10 +0100

    battle page route and import

--- src/page/index.js
+++ src/page/index.js
@@ -1,10 +1,12 @@
 import CreateBattle from "./CreateBattle";
 import JoinBattle from "./JoinBattle";
+import Battle from "./Battle";
 import Home from "./Home";
 
 
 export {
     Home,
     CreateBattle,
-    JoinBattle
+    JoinBattle,
+    Battle
 }
